04cc7f7d06a5f196f99b18844c8184b1a8091a0c
Jihee <115629285+Anjihee@users.noreply.github.com>
2025-03-17 12:40:18 +0900

    Initial Commit

---
+++ Dailist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dailist
+//
+//  Created by 안지희 on 3/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DailistTests/DailistTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailistTests.swift
+//  DailistTests
+//
+//  Created by 안지희 on 3/17/25.
+//
+
+import Testing
+@testable import Dailist
+
+struct DailistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
